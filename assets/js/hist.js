68a5042e6fff7d1890cc8e0b4b7fd63aacaf8abe
ajarifi <53268007+ajarifi@users.noreply.github.com>
2022-01-16 22:27:50 +0900

    Update hist.js

--- assets/js/hist.js
+++ assets/js/hist.js
@@ -5,23 +5,23 @@ function drawBasic() {
 
 			var data = google.visualization.arrayToDataTable([
          ['Year', 'Paper' ],
-         ['15', 1],
-         ['16', 2],
-         ['17', 2],
-         ['18', 1 ],
-         ['19', 1 ],
-         ['20', 3],
-         ['21', 2],
-         ['22', 0],
-         ['23', 0],
-         ['24', 0],
-         ['25', 0],
-         ['26', 0],
+         ['Y15', 1],
+         ['Y16', 2],
+         ['Y17', 2],
+         ['Y18', 1 ],
+         ['Y19', 1 ],
+         ['Y20', 3],
+         ['Y21', 2],
+         ['Y22', 0],
+         ['Y23', 0],
+         ['Y24', 0],
+         ['Y25', 0],
+         ['Y26', 0],
       ]);
 
       var options = {
       	legend: 'none',
-        title: 'Publication (from 2015)'
+        title: 'Publication each year'
       };
 
       var chart = new google.visualization.ColumnChart(
