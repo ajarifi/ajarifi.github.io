1b525abc638005604832fa0fff14b4e0b48ce1ba
ajarifi <53268007+ajarifi@users.noreply.github.com>
2022-01-16 20:18:33 +0900

    Update pie.js

--- assets/js/pie.js
+++ assets/js/pie.js
@@ -1,18 +1,22 @@
-google.charts.load('current', {packages: ['corechart']});
+google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-  // Define the chart to be drawn.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Element');
-  data.addColumn('number', 'Percentage');
-  data.addRows([
-    ['Nitrogen', 0.78],
-    ['Oxygen', 0.21],
-    ['Other', 0.01]
+
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work',     11],
+    ['Eat',      2],
+    ['Commute',  2],
+    ['Watch TV', 2],
+    ['Sleep',    7]
   ]);
 
-  // Instantiate and draw the chart.
-  var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
-  chart.draw(data, null);
+  var options = {
+    title: 'My Daily Activities'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, options);
 }
