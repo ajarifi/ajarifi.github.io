fbdf6d2a7d0895e593ab3a8cca6f5fea85f4c1a0
ajarifi <53268007+ajarifi@users.noreply.github.com>
2022-01-17 15:06:02 +0900

    Update japan.js

--- assets/js/japan.js
+++ assets/js/japan.js
@@ -1,8 +1,29 @@
 var d = new jpmap.japanMap(document.getElementById("my-map"), {
   areas: [
-    { "code":1, "color":"#AA0000" }, 
-    { "code":2, "color":"#00AA00" }
+    { "code":4, "color":"#0000FF" },
+    { "code":5, "color":"#0000FF" },
+    { "code":8, "color":"#0000FF" },    
+    { "code":9, "color":"#0000FF" },
+    { "code":10, "color":"#0000FF" }, 
+    { "code":11, "color":"#0000FF" },
+    { "code":12, "color":"#0000FF" },    
+    { "code":13, "color":"#0000FF" },
+    { "code":17, "color":"#0000FF" }, 
+    { "code":23, "color":"#0000FF" },
+    { "code":26, "color":"#0000FF" }, 
+    { "code":27, "color":"#0000FF" },
+    { "code":28, "color":"#0000FF" }, 
+    { "code":29, "color":"#0000FF" },
+    { "code":30, "color":"#0000FF" }, 
+    { "code":33, "color":"#0000FF" },
+    { "code":34, "color":"#0000FF" },
+    { "code":35, "color":"#0000FF" },
+    { "code":40, "color":"#0000FF" },
+    { "code":45, "color":"#0000FF" }
   ],
+  showsPrefectureName: true,
+  movesIslands: true,
+  lang: 'en',
   borderLineColor: "#000000", 
   onSelect: function(data){
     alert(data.name);
