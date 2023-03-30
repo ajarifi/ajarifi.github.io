354f7d95df890aa2a15e30a3ed38f51086ff40f2
ajarifi <53268007+ajarifi@users.noreply.github.com>
2023-03-30 20:04:06 +0900

    Update hist.js

--- assets/js/hist.js
+++ assets/js/hist.js
@@ -1,52 +1,46 @@
-google.charts.load('current', { packages: ['corechart', 'bar'] });
+google.charts.load('current', {packages: ['corechart', 'bar']});
 google.charts.setOnLoadCallback(drawBasic);
 
-const drawBasic = () => {
-  const data = google.visualization.arrayToDataTable([
-    ['Year', 'Paper'],
-    ['15', 1],
-    [' ', 2],
-    [' ', 2],
-    [' ', 1],
-    [' ', 1],
-    ['20', 3],
-    [' ', 2],
-    [' ', 3],
-    [' ', 1],
-    [' ', 0],
-    ['25', 0],
-    [' ', 0],
-    [' ', 0],
-    [' ', 0],
-    [' ', 0],
-    ['30', 0],
-    [' ', 0],
-    [' ', 0],
-    [' ', 0],
-    [' ', 0],
-    ['35', 0],
-    [' ', 0],
-    [' ', 0],
-    [' ', 0],
-    [' ', 0],
-    ['  ', 0],
-  ]);
+function drawBasic() {
 
-  const options = {
-    legend: 'none',
-    title: 'Publication each year',
-    bar: { groupWidth: '95%' },
-    vAxis: {
-      textPosition: 'in',
-      gridlines: {
-        color: 'transparent',
-      },
-    },
-  };
+			var data = google.visualization.arrayToDataTable([
+         ['Year', 'Paper' ],
+         ['15', 1],
+         [' ', 2],
+         [' ', 2],
+         [' ', 1],
+         [' ', 1],
+         ['20', 3],
+         [' ', 2],
+         [' ', 3],
+         [' ', 1],
+         [' ', 0],
+         ['25', 0],
+         [' ', 0],
+         [' ', 0],
+         [' ', 0],
+         [' ', 0],
+         ['30', 0],
+         [' ', 0],	
+         [' ', 0],
+         [' ', 0],
+         [' ', 0],
+         ['35', 0],
+         [' ', 0],
+         [' ', 0],
+         [' ', 0],
+         [' ', 0],
+         ['  ', 0]
+      ]);
 
-  const chart = new google.visualization.ColumnChart(
-    document.getElementById('chart_div')
-  );
+      var options = {
+      	legend: 'none',
+        title: 'Publication each year',
+	bar: {groupWidth: "95%"},
+      };
 
-  chart.draw(data, options);
-};
+      var chart = new google.visualization.ColumnChart(
+        document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
