ec0805e3e1a9c3354ab7e1aec4906f86c395abf5
ajarifi <53268007+ajarifi@users.noreply.github.com>
2023-03-30 19:59:24 +0900

    Update hist.js

--- assets/js/hist.js
+++ assets/js/hist.js
@@ -1,46 +1,52 @@
-google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.load('current', { packages: ['corechart', 'bar'] });
 google.charts.setOnLoadCallback(drawBasic);
 
-function drawBasic() {
+const drawBasic = () => {
+  const data = google.visualization.arrayToDataTable([
+    ['Year', 'Paper'],
+    ['15', 1],
+    [' ', 2],
+    [' ', 2],
+    [' ', 1],
+    [' ', 1],
+    ['20', 3],
+    [' ', 2],
+    [' ', 3],
+    [' ', 1],
+    [' ', 0],
+    ['25', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    ['30', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    ['35', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    ['  ', 0],
+  ]);
 
-			var data = google.visualization.arrayToDataTable([
-         ['Year', 'Paper' ],
-         ['15', 1],
-         [' ', 2],
-         [' ', 2],
-         [' ', 1],
-         [' ', 1],
-         ['20', 3],
-         [' ', 2],
-         [' ', 3],
-         [' ', 1],
-         [' ', 0],
-         ['25', 0],
-         [' ', 0],
-         [' ', 0],
-         [' ', 0],
-         [' ', 0],
-         ['30', 0],
-         [' ', 0],	
-         [' ', 0],
-         [' ', 0],
-         [' ', 0],
-         ['35', 0],
-         [' ', 0],
-         [' ', 0],
-         [' ', 0],
-         [' ', 0],
-         ['  ', 0]
-      ]);
+  const options = {
+    legend: 'none',
+    title: 'Publication each year',
+    bar: { groupWidth: '95%' },
+    vAxis: {
+      textPosition: 'in',
+      gridlines: {
+        color: 'transparent',
+      },
+    },
+  };
 
-      var options = {
-      	legend: 'none',
-        title: 'Publication each year',
-	bar: {groupWidth: "95%"},
-      };
+  const chart = new google.visualization.ColumnChart(
+    document.getElementById('chart_div')
+  );
 
-      var chart = new google.visualization.ColumnChart(
-        document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-    }
+  chart.draw(data, options);
+};
