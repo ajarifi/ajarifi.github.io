da422931bc69d00a768c087d2566081349d20f30
Ahmad Jafar Arifi <53268007+ajarifi@users.noreply.github.com>
2024-10-21 19:04:15 +0900

    Create hist2.js

---
+++ assets/js/hist2.js
@@ -0,0 +1,60 @@
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+  var data = google.visualization.arrayToDataTable([
+    ['Year', 'Paper' ],
+    ['2015', 1],
+    [' ', 2],
+    [' ', 2],
+    [' ', 1],
+    [' ', 1],
+    [' ', 3],
+    [' ', 2],
+    [' ', 3],
+    [' ', 2],
+    [' ', 5],
+    ['2025', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],	
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    ['2035', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    ['2045', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    [' ', 0],
+    ['2055', 0]
+  ]);
+
+  var options = {
+    legend: 'none',
+    bar: {groupWidth: "100%"},
+    colors: ['#77C3EC'],
+  };
+
+  var chart = new google.visualization.ColumnChart(
+    document.getElementById('chart_div_talk'));
+
+  chart.draw(data, options);
+}
